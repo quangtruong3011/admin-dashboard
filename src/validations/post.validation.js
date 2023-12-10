9fadffec583673e973f799c62250ee392e33d442
quangtruong3011 <lequangtruong3011@gmail.com>
2023-12-10 20:01:52 +0700

    update

--- src/validations/post.validation.js
+++ src/validations/post.validation.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
 export const postValidationSchema = Yup.object().shape({
-    title: Yup.string().required("Title is required"),
+    title: Yup.string().max(255, "Title must be at most 255 characters").required("Title is required"),
     content: Yup.string().required("Content is required"),
 });
--- src/validations/product.validation.js
+++ src/validations/product.validation.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const productValidationSchema = Yup.object().shape({
-    productName: Yup.string().required("Product Name is required"),
+    productName: Yup.string().axt(255, "Product Name must be at most 255 characters").required("Product Name is required"),
     category: Yup.string().required("Category is required"),
     unit: Yup.string().required("Unit is required"),
     description: Yup.string().required("Description is required"),
